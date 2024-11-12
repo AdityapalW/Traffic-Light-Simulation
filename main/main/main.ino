83c497d9e8bb947fb81cf4d796516969559763ef
sweeezin <87355092+sweeezin@users.noreply.github.com>
2024-11-12 12:11:50 -0800

    Pin declaration for inputs
    for main

--- main/main/main.ino
+++ main/main/main.ino
@@ -1,3 +1,23 @@
+//define pins
+
+  //rain sensor
+const int rain1Pin = 1;
+  //button
+const int buttonPin = 3;
+  //motion sensor
+const int motionPin = 4;
+  //lights
+const int redLightPin = 5;
+const int yellowLightPin = 6;
+const int greenLightPin = 7;
+
+const int pedLight1Pin = 8;
+const int pedLight2Pin = 9;
+  //speaker
+const int speakerPin = 10;
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
