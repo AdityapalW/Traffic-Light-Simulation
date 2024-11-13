2f9af408c9f648af6a7e821caab83d71c6d59292
sweeezin <87355092+sweeezin@users.noreply.github.com>
2024-11-12 16:43:41 -0800

    Setup inputs and outputs in setup function

--- main/main/main.ino
+++ main/main/main.ino
@@ -1,29 +1,51 @@
 //define pins
 
-  //rain sensor
+//rain sensor
 const int rain1Pin = 1;
-  //button
+
+//button
 const int buttonPin = 3;
-  //motion sensor
+
+//motion sensor
 const int motionPin = 4;
-  //lights
+
+//lights
 const int redLightPin = 5;
 const int yellowLightPin = 6;
 const int greenLightPin = 7;
 
 const int pedLight1Pin = 8;
 const int pedLight2Pin = 9;
-  //speaker
-const int speakerPin = 10;
+
+const int speakerPin = 10; //speaker
 
 
+//setup function
 
 void setup() {
-  // put your setup code here, to run once:
+  // Inputs
+  pinMode(rain1Pin, INPUT);    
+  pinMode(buttonPin, INPUT);   
+  pinMode(motionPin, INPUT);
+  //Outputs
+  pinMode(redLightPin, OUTPUT);
+  pinMode(yellowLightPin, OUTPUT);
+  pinMode(greenLightPin, OUTPUT);
+  pinMode(pedLight1Pin, OUTPUT);
+  pinMode(pedLight2Pin, OUTPUT);
+  pinMode(speakerPin, OUTPUT);
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  //match pin to input value
+  int rain1 = digitalRead(rain1Pin);
+  int button = digitalRead(buttonPin); //needs timer?
+  int motion = digitalRead(motionPin);
+
+  //make array made of input values
+
 
 }
