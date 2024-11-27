eed0b98eb63f1604819084cc034b6b84a94f89fe
sweeezin <87355092+sweeezin@users.noreply.github.com>
2024-11-26 16:58:14 -0800

    fix tone function commit

--- main/main/main.ino
+++ main/main/main.ino
@@ -17,7 +17,7 @@ const int greenLightPin = 8;
 const int pedLight1Pin = 9;  //Walking Sign
 const int pedLight2Pin = 10; //hand sign
 
-const int speakerPin = 11; //speaker
+const int spkPin = 11; //speaker
 
  int waitTime;
  int crossTime
@@ -27,9 +27,7 @@ const int speakerPin = 11; //speaker
 int buttonState = LOW;           // Variable to hold the button state
 int motionDetected = LOW;        // Variable to hold motion sensor state
 unsigned long buttonPressTime = 0; // To record the time when the button is pressed
-int tones[] = {
-  3465, 2850, 2333, 1956, 1638, 1380, 1161, 992, 814, 704, 500
-};
+
 
 void setup() {
   // Inputs
@@ -42,7 +40,7 @@ void setup() {
   pinMode(greenLightPin, OUTPUT);
   pinMode(pedLight1Pin, OUTPUT);
   pinMode(pedLight2Pin, OUTPUT);
-  pinMode(speakerPin, OUTPUT);
+  pinMode (spkPin, OUTPUT);
 
 
   //initialise all the Lights
@@ -52,11 +50,26 @@ void setup() {
   digitalWrite(pedLight1Pin, LOW);
   digitalWrite(pedLight2Pin, HIGH);
 
-  void playIdle() {
+
+
+  // ignore this 
+  Serial.begin(9600);                 // Start serial communication for debugging
+
+
+}
+
+//functions for playing tones (speaker)
+
+int tones[] = {
+  3465, 2850, 2333, 1956, 1638, 1380, 1161, 992, 814, 704, 500
+};
+
+void playIdle() {
   tone(spkPin, 973, 25);
   delay(25);
   noTone(spkPin);
 }
+
 void playChirp() {
   // Iterate through all tones
   for (int i = 0; i < 11; i++) {
@@ -64,6 +77,7 @@ void playChirp() {
     tone(spkPin, tones[i], 11);
     delay(11);
   }
+
 }
 void playWoodpecker() {
   // 17x = 2secs; 85x = 10 seconds
@@ -75,48 +89,35 @@ void playWoodpecker() {
   }
 }
 
-
-  // ignore this 
-  Serial.begin(9600);                 // Start serial communication for debugging
-
-
-}
-
 void loop() {
 
-  //match pin to input value
-  int rain1value = analogRead(A0); //Since rain sensor is a analog sensor
-  buttonState = digitalRead(buttonPin); //needs timer?
-  motionDetected = digitalRead(motionPin);
+  // Read the state of the button
+ int  buttonState = digitalRead(buttonPin);
 
-  //make array made of input values
+  if (buttonState == LOW) { // Button pressed (LOW with pull-up)
+    Serial.println("Button pressed, starting sequence...");
+    buttonPressTime = millis(); // Record the time button was pressed
+    playChirp();
+    // Wait for 3 seconds
+    delay(3000);
 
-
-
-
-  // If the button is pressed
-  if (buttonState == LOW) {
-    Serial.println("Button pressed. Pedestrian crossing requested.");
-   playChirp();
-
-  if (rain1value > 500) { // Threshold for detecting rain (adjust as needed)
-      Serial.println("Rain detected. Adjusting timings...");
-      waitTime = 5000;   // Shorten pedestrian wait time
-      crossTime = 8000;  // Extend pedestrian crossing time
-    } else {
-      Serial.println("No rain detected. Using default timings.");
-      waitTime = 10000;  // Default wait time
-      crossTime = 5000;  // Default crossing time
-    }
-    delay(waitTime/2);
-
+    // Check for motion
+    motionDetected = digitalRead(motionPin);
     if (motionDetected == HIGH) {
-      Serial.println("Motion detected. Pedestrian is still here");
+      Serial.println("Motion detected! Continuing...");
+    } else {
+      Serial.println("No motion detected! Light will not turn on.");
+      return; // Stop further actions if no motion
+    }
 
-    delay(waitTime/2);
+    // Wait until 10 seconds have passed since the button was pressed
+    while (millis() - buttonPressTime < 10000) {
+      // Do nothing, just wait
+    }
 
-      // After the wait time is donr turn the light red
-      digitalWrite(greenLightPin, LOW);
+    // Turn on the light
+    Serial.println("10 seconds passed, turning on light.");
+         digitalWrite(greenLightPin, LOW);
       digitalWrite(yellowLightPin, HIGH);
       delay(3000); // Yellow light delay
       digitalWrite(yellowLightPin, LOW);
@@ -125,25 +126,17 @@ void loop() {
       // Pedestrian light ON
       digitalWrite(pedLight1Pin, HIGH);
       digitalWrite(pedLight2Pin, LOW);
-
-
-      //let the padestrian cross
-
-      delay(crossTime);
-
-
-      //reseting all the light
+      playChirp();
+    // Keep the light on for demonstration; you can modify this part as needed
+    delay(5000); // Light stays on for 5 seconds (for testing purposes)
       digitalWrite(redLightPin, LOW);
       digitalWrite(greenLightPin, HIGH);
       digitalWrite(pedLight1Pin, LOW);
       digitalWrite(pedLight2Pin, HIGH);
-
-      Serial.println("Crossing complete. Lights reset.");
-
-  }  else {  // if no motion was detected
-      Serial.println("No motion detected. Cancelling crossing.");
+    Serial.println("Light turned off.");
   }
-
-
-
+  else {
+    Serial.println("Button not pressed");
+  }
+  //delay(1000);
 }
